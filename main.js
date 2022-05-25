5a4c3780b144de743d106ad42f3878b3365481ba
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-05-25 18:58:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 leftwristx = "";
 rightwristx = "";
+difference = "";
 
 function setup() {
     canvas = createCanvas(550, 475);
@@ -12,19 +13,23 @@ function setup() {
     posenet.on("pose", getposes);
 }
 
+function modelloaded() {
+    console.log("Model Successfully Loaded");
+}
+
 function getposes(result) {
     if (result.length > 0) {
         console.log(result);
         leftwristx = result[0].pose.leftWrist.x;
         rightwristx = result[0].pose.rightWrist.x;
-        console.log("Left Wrist X: " + leftwristx + ", " + "Right Wrist X: " + rightwristx);
+        difference = floor(leftwristx - rightwristx)
+        console.log("Left Wrist X: " + leftwristx + ", " + "Right Wrist X: " + rightwristx + ", " + "Difference: " + difference);
     }
-}
-
-function modelloaded() {
-    console.log("Model Successfully Loaded");
 }
 
 function draw() {
     background("#04a6eb");
+    textSize(difference);
+    fill("#FFE787")
+    text("Adarsh", 50, 400);
 }
